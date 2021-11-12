581d24771e2913f4ab71e19b670d7ec00bce629e
reevest2 <reevest2@gmail.com>
2021-11-11 19:49:27 -0500

    Filled out more skills (need to make better) - added about me page

--- Controllers/HomeController.cs
+++ Controllers/HomeController.cs
@@ -23,6 +23,11 @@ namespace PersonalSite.Controllers
             return View();
         }
 
+        public IActionResult About()
+        {
+            return View();
+        }
+
         public IActionResult Projects()
         {
             return View();
